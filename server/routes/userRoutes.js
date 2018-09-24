5a798f101e5a76ed269bfadef398780036172a37
himanshu savita <himanshusavita@himanshus-Air.gateway-fw.fw>
2018-09-24 21:00:15 +0530

    pagination added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,7 +11,6 @@ module.exports = app =>{
                password : req.body.password
            }
            let signup = await api.post('signup', accountDetails);
-           console.log(signup);
          }
          res.render('index.hbs');
      }
@@ -21,10 +20,18 @@ module.exports = app =>{
 
     app.get('/book-list/:paginationcount?', async (req ,res)=>{
         let paginationCount = req.params.paginationcount || 0, prev=0, next=0;
-            if(paginationCount>1){
+        let columnCounts = 6, pageCountStart = 1, pageCountEnd = 6;
+            if(paginationCount>1)
                prev = parseInt(paginationCount)-1;
-            }
             next = parseInt(paginationCount)+1;
+            let columnCountsJSON = {};
+            if(paginationCount>6){
+               pageCountStart = paginationCount;
+               pageCountEnd = parseInt(paginationCount)+columnCounts;
+            }
+            for(let i= pageCountStart; i<=pageCountEnd; i++) {
+                 columnCountsJSON[i] = { count: i,  url: '/book-list/'+i }
+            }
             limit = 12;
         let getAllBooks = await api.get('books',{
             limit : limit,
@@ -39,7 +46,8 @@ module.exports = app =>{
         console.log(getAllBooks);
         let pagination = {
             'prev': prev,
-            'next': next
+            'next': next,
+            columnCountsJSON
         }
         res.render('book-grid-wrs.hbs', { data : renderData, pagination });
     })
